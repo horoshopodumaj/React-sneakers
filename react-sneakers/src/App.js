4ac248518e732560e99b64e59ff7926e062fa608
Diana Murtazina <murka8831@yandex.ru>
2022-09-29 14:03:41 +0400

    project markup

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,5 +1,29 @@
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="wrapper">
+            <header>
+                <div className="headerLeft">
+                    <img width={40} height={40} src="./img/logo.png" />
+                    <div className="headerInfo">
+                        <h3>react sneakers</h3>
+                        <p>Магазин лучших кроссовок</p>
+                    </div>
+                </div>
+                <ul className="headerRight">
+                    <li>
+                        <svg />
+                        <span>1205 руб</span>
+                    </li>
+                    <li>
+                        <svg />
+                    </li>
+                </ul>
+            </header>
+            <div className="content">
+                <h1>все кроссовки</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;

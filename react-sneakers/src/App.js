fa5daa53ecc8ca6849840c38c0db09b887301587
Diana Murtazina <murka8831@yandex.ru>
2022-09-30 12:33:31 +0400

    adding styles for header

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,17 +1,27 @@
 function App() {
     return (
         <div className="wrapper clear">
-            <header className="d-flex justify-between">
+            <header className="d-flex justify-between align-center p-40">
                 <div className="d-flex align-center">
-                    <img width={40} height={40} src="./img/logo.png" />
-                    <div className="headerInfo">
-                        <h3>react sneakers</h3>
+                    <img
+                        className="mr-15"
+                        width={40}
+                        height={40}
+                        src="./img/logo.png"
+                    />
+                    <div>
+                        <h3 className="text-uppercase">react sneakers</h3>
                         <p>Магазин лучших кроссовок</p>
                     </div>
                 </div>
-                <ul className="headerRight">
-                    <li>
-                        <img width={18} height={18} src="./img/cart.svg" />
+                <ul className="d-flex">
+                    <li className="mr-30">
+                        <img
+                            className="mr-15"
+                            width={18}
+                            height={18}
+                            src="./img/cart.svg"
+                        />
                         <span>1205 руб</span>
                     </li>
                     <li>
@@ -19,7 +29,7 @@ function App() {
                     </li>
                 </ul>
             </header>
-            <div className="content">
+            <div className="content p-40">
                 <h1>все кроссовки</h1>
             </div>
         </div>

a13c34c9334123cc27de3b0cb8e5e811005552d8
Diana Murtazina <murka8831@yandex.ru>
2022-10-16 18:36:56 +0400

    added component header

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,4 +1,5 @@
 import Card from "./components/Card";
+import Header from "./components/Header";
 
 function App() {
     return (
@@ -75,34 +76,7 @@ function App() {
                     </div>
                 </div>
             </div>
-            <header className="d-flex justify-between align-center p-40">
-                <div className="d-flex align-center">
-                    <img
-                        className="mr-15"
-                        width={40}
-                        height={40}
-                        src="./img/logo.png"
-                    />
-                    <div>
-                        <h3 className="text-uppercase">react sneakers</h3>
-                        <p className="opacity-5">Магазин лучших кроссовок</p>
-                    </div>
-                </div>
-                <ul className="d-flex">
-                    <li className="mr-30">
-                        <img
-                            className="mr-15"
-                            width={18}
-                            height={18}
-                            src="./img/cart.svg"
-                        />
-                        <span>1205 руб</span>
-                    </li>
-                    <li>
-                        <img width={18} height={18} src="./img/user.svg" />
-                    </li>
-                </ul>
-            </header>
+            <Header />
             <div className="content p-40">
                 <div className="d-flex align-center mb-40 justify-between">
                     <h1>Все кроссовки</h1>
